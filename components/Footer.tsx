702c787c658b178af45236ad347b27b1044317f7
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-08-24 21:33:53 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,11 +5,11 @@ export default function Footer() {
     return (
       <div className="absolute bottom-0 py-6  w-screen flex items-center justify-center border-t border-neutral-500">
           <h3 className="text-sm">
-            © 2021 Psytech Studio. All Rights Reserved. | Designed In Cape Town
+            2023 Psytech Studio | Designed In Cape Town
           </h3>
           <div className="absolute right-0">
             <Link target="_blank" href={'https://github.com/christiaan-fourie/tintio'}>
-                <Image src="/github.svg" alt="Github Repo" width={50} height={50} />    
+                <Image src="/github.svg" alt="Github Repo" width={50} height={50} />
             </Link>            
           </div>
       </div>    
